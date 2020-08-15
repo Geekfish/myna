58eab5279e06af61ed54653739b72abc54c31aae
Eleni Lixourioti <contact@eleni.co>
2020-08-15 22:54:04 +0200

    Only initialise the RTM server when the token is available

--- lib/myna.ex
+++ lib/myna.ex
@@ -5,6 +5,31 @@ defmodule Myna do
 
   def start(_start_type, _start_args) do
     IO.puts("Starting the application")
-    Slack.Bot.start_link(Myna.SlackRtm, [], Application.get_env(:slack, :api_token))
+
+    children =
+      :slack
+      |> Application.get_env(:api_token)
+      |> children()
+
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
+
+  @doc """
+  Used only for initial tests
+  """
+  def hello(), do: :world
+
+  defp children(nil) do
+    IO.puts("Slack Bot not started: api token not configured")
+    []
+  end
+
+  defp children(token) do
+    [
+      %{
+        id: Slack.Bot,
+        start: {Slack.Bot, :start_link, [Myna.SlackRtm, [], token]}
+      }
+    ]
   end
 end
